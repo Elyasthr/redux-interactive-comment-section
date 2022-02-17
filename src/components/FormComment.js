364fd79ba52c1b5b0fb645ad12f48c48f10cb731
Elyas <touahria.elyas@outlook.com>
2022-02-17 11:26:15 +0100

    delete confirmation block

--- src/components/FormComment.js
+++ src/components/FormComment.js
@@ -2,57 +2,61 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { postComment } from '../store/actions/comment.action';
 import { getComment, putComment } from '../store/actions/comment.action';
+import { isEmpty } from './Utils';
 
 const FormComment = ({ reply, comment }) => {
-
   const user = useSelector((state) => state.userReducer);
   const [myReply, setMyReply] = useState('');
   const dispatch = useDispatch()
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const dataComment = {
-      content: myReply,
-      createdAt: Date.now(),
-      score: 0,
-      user: {
-        image: {
-          png: "./images/avatars/image-juliusomo.png",
-          webp: "./images/avatars/image-juliusomo.webp"
+    if (!isEmpty(myReply)) {
+      const dataComment = {
+        content: myReply,
+        createdAt: Date.now(),
+        score: 0,
+        user: {
+          image: {
+            png: "./images/avatars/image-juliusomo.png",
+            webp: "./images/avatars/image-juliusomo.webp"
+          },
+          username: user.username
         },
-        username: user.username
-      },
-      replies: []
+        replies: []
+      }
+      await dispatch(postComment(dataComment)) //Waiting post to register
+      dispatch(getComment())
+      setMyReply('');
     }
-    await dispatch(postComment(dataComment)) //Waiting post to register
-    dispatch(getComment())
-    setMyReply('');
   }
   //fix Id problem
   const handleReply = async (e) => {
     e.preventDefault();
-    const dataReply = {
-      id: comment.replies.length + 1,
-      content: myReply,
-      createdAt: Date.now(),
-      score: 0,
-      replyingTo: reply ? reply.user.username : comment.user.username,
-      user: {
-        image: {
-          png: "./images/avatars/image-amyrobson.png",
-          webp: "./images/avatars/image-amyrobson.webp"
-        },
-        username: user.username
+    if (!isEmpty(myReply)) {
+      const dataReply = {
+        id: comment.replies.length + 1,
+        content: myReply,
+        createdAt: Date.now(),
+        score: 0,
+        replyingTo: reply ? reply.user.username : comment.user.username,
+        user: {
+          image: {
+            png: "./images/avatars/image-amyrobson.png",
+            webp: "./images/avatars/image-amyrobson.webp"
+          },
+          username: user.username
+        }
       }
-    }
 
-    const dataCommentEdit = {
-      ...comment,
-      replies: [...comment.replies, dataReply]
+      const dataCommentEdit = {
+        ...comment,
+        replies: [...comment.replies, dataReply]
+      }
+      await dispatch(putComment(dataCommentEdit))
+      dispatch(getComment())
+      setMyReply('');
     }
-    await dispatch(putComment(dataCommentEdit))
-    dispatch(getComment())
-    setMyReply('');
   }
 
   return (
